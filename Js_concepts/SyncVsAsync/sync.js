b3828a36ea98d1e68e03d98c95c83057e62496e1
Dmusketeer <ardmountronics@gmail.com>
2021-10-13 09:01:51 +0530

    sync

---
+++ Js_concepts/SyncVsAsync/sync.js
@@ -0,0 +1,12 @@
+// In a synchronous system, tasks are completed one after another.
+
+console.log("I");
+console.log("AM");
+console.log("In");
+console.log("LOVE");
+console.log("WITH");
+console.log("Choco");
+console.log("What");
+console.log("about");
+console.log("you");
+console.log("!");
