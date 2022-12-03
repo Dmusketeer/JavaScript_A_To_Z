9e7e2f610300183fe8363f812ad3e13a52e590b7
Dmuskeeter <tdheeraj9a@gmail.com>
2022-12-03 15:23:39 +0530

    practice

--- 50PracticeQuestions/practice.js
+++ 50PracticeQuestions/practice.js
@@ -1,4 +1,23 @@
-// Print numbers from 1 to 10
-for(let i=1;i<=10;i++){
-    console.log(i)
+//1. Print numbers from 1 to 10
+// for(let i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+
+
+// 2.Print the odd numbers less than 100
+
+for(let i=1;i<100;i++){
+    if(i%2!=0){
+        console.log(i)
+    }
 }
+
+
+
+
+
+
+
+
+
