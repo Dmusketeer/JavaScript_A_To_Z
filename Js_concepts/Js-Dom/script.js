f7294a2e06896b352e6b2e8a36579554ab4bdbef
Dmuskeeter <tdheeraj9a@gmail.com>
2023-01-08 18:55:06 +0530

    js Dom

---
+++ Js_concepts/Js-Dom/script.js
@@ -0,0 +1,17 @@
+// var mynameTarget = document.body;
+// mynameTarget.style.backgroundImage = "linear-gradient(to right,black,white)";
+// var loremTarget = document.getElementsByTagName("p");
+// var namecolor = document.getElementById("myName");
+// namecolor.style.color = "white";
+// // loremTarget.style.backgroundColor = "red";
+
+// for (let i = 0; i <= loremTarget.length; i++) {
+//   loremTarget[i].style.color = "red";
+// }
+// console.log(mynameTarget);
+
+var mainContentTarget = document.querySelectorAll(".spanClass span");
+// mainContentTarget.style.backgroundColor = "red";
+for (let i = 0; i <= mainContentTarget.length; i++) {
+  mainContentTarget[i].style.color = "red";
+}
