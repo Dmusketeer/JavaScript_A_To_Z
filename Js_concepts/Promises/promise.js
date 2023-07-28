39f39a81dedb843b1093596e66f5e7da5a8f5035
Dmuskeeter <tdheeraj9a@gmail.com>
2023-07-28 22:22:09 +0530

    promise

--- Js_concepts/Promises/promise.js
+++ Js_concepts/Promises/promise.js
@@ -1,31 +1,35 @@
-
 // Creating a promise
 
-let GoodMarks = false
+let GoodMarks = false;
 // producing Code
 
 // producing + consuming code
 let birthdayCelebrations = new Promise((resolve, reject) => {
-    if (GoodMarks) {
-        resolve('you\'ll get a car on your birthday')
-    } else {
-        reject('your marks is not good so you\'ll not get a car on your birthday')
+  if (GoodMarks) {
+    resolve("you'll get a car on your birthday");
+  } else {
+    reject("your marks is not good so you'll not get a car on your birthday");
+  }
+})
+  .then(
+    (value) => {
+      console.log(value);
     }
-}).then(
-    (value) => { console.log(value) }
     // (err) => { console.log(err) }
-).catch(
-    (err) => { console.log(err) }
-).finally(() => { console.log('finally done promise') })
-
+  )
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log("finally done promise");
+  });
 
 // consuming code
 // birthdayCelebrations.then(
 //     (value) => { console.log(value) }
-//     // (err) => { console.log(err) }
+// (err) => { console.log(err) }
 // ).catch(
 //     (err) => { console.log(err) }
 // ).finally(() => { console.log('finally done promise') })
-
 
 // Consuming a Promise: then, catch, finally
